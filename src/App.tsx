bab74cf97abcc3355e5f613b7817131b864c7846
wylken <wylken.ufal@gmail.com>
2023-04-08 17:23:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <h1>PROJETO BASE FUNCIONANDO...</h1>
+    </ThemeProvider>
+  )
+}
